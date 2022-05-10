248c0a5f2ceded65ce898d7a6483373d8f5b3b6d
adrianrufino <98433626+adrianrufino@users.noreply.github.com>
2022-05-10 15:23:14 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,58 +1,58 @@
-let arrayCitas = [];
+// let arrayCitas = [];
 
-//DEFINICION DE CLASES
+// //DEFINICION DE CLASES
 
-class Persona {
-    constructor(nombre,apellido,telefono){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-    }
-}
+// class Persona {
+//     constructor(nombre,apellido,telefono){
+//         this.nombre = nombre;
+//         this.apellido = apellido;
+//         this.telefono = telefono;
+//     }
+// }
 
-class Cita {
-    constructor(fecha,hora,persona){
-        this.fecha = fecha;
-        this.hora = hora;
-        this.persona = persona;
-    }
-}
+// class Cita {
+//     constructor(fecha,hora,persona){
+//         this.fecha = fecha;
+//         this.hora = hora;
+//         this.persona = persona;
+//     }
+// }
 
-let persona;
+// let persona;
 
-//HICE EL FOR PARA COMPLETAR MI VECTOR/ARRAY DE CITAS.
+// //HICE EL FOR PARA COMPLETAR MI VECTOR/ARRAY DE CITAS.
 
-for (let i=0; i<= 2; i++){
-    altaPersona();
-    altaCita();
-}
-console.log(arrayCitas)
+// for (let i=0; i<= 2; i++){
+//     altaPersona();
+//     altaCita();
+// }
+// console.log(arrayCitas)
 
 
-//DEFINICION DE FUNCIONES
+// //DEFINICION DE FUNCIONES
 
-function altaPersona() {
-    let nombre = prompt("Ingrese su nombre");
-    let apellido = prompt("Ingrese su apellido");
-    let telefono = prompt("Ingrese su telefono de Contacto");
-    persona = new Persona(nombre,apellido,telefono);
+// function altaPersona() {
+//     let nombre = prompt("Ingrese su nombre");
+//     let apellido = prompt("Ingrese su apellido");
+//     let telefono = prompt("Ingrese su telefono de Contacto");
+//     persona = new Persona(nombre,apellido,telefono);
     
-}
+// }
 
 
 
-function altaCita() {
-    let fecha = prompt("Ingrese la fecha");
-    let hora = prompt("Ingrese la Hora");
-    let cita = new Cita(fecha,hora,persona);
-    arrayCitas.push(cita);
-}
+// function altaCita() {
+//     let fecha = prompt("Ingrese la fecha");
+//     let hora = prompt("Ingrese la Hora");
+//     let cita = new Cita(fecha,hora,persona);
+//     arrayCitas.push(cita);
+// }
 
 
-function buscarFecha(fecha){
-    return arrayCitas.find(a =>a.fecha == fecha);
-}
+// function buscarFecha(fecha){
+//     return arrayCitas.find(a =>a.fecha == fecha);
+// }
 
-function buscarApellido(apellido){
-    return arrayCitas.find(a =>a.apellido == apellido);
-}
+// function buscarApellido(apellido){
+//     return arrayCitas.find(a =>a.apellido == apellido);
+// }
